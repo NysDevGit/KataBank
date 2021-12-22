cd8844bd27dd2583f4fb4ce334fa017f4f509c89
Younouss <toshiroxx@hotmail.fr>
2021-12-22 17:04:42 +0100

    add test

--- src/test/java/BankServiceTest.java
+++ src/test/java/BankServiceTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 public class BankServiceTest {
 
     @Nested
-    class Deposit{
+    class DepositTest{
 
         @Test
-        public void should_add_deposit_amount_to_current_account_balance(){
+        public void should_add_amount_to_current_account_balance(){
             //Given
             BankService bankService = new BankService();
             Account account = new Account(new Balance(100));
@@ -21,5 +21,23 @@ public class BankServiceTest {
             Assertions.assertEquals(150.0, bankService.getAccountBalance(account));
 
         }
+
+        @Test
+        public void should_add_severals_amount_to_current_account_balance(){
+            //Given
+            BankService bankService = new BankService();
+            Account account = new Account(new Balance(20));
+            double amountToDeposit = 50;
+            double secondAmountToDeposit = 130;
+
+            //When
+            bankService.deposit(account, amountToDeposit);
+            bankService.deposit(account, secondAmountToDeposit);
+
+            //Then
+            Assertions.assertEquals(200.0, bankService.getAccountBalance(account));
+
+        }
     }
+
 }
